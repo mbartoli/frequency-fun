d09b1fdbfe2f38cd90a8b9accb99e67fd8d3323c
Noah Mulfinger <noah.mulfinger@gmail.com>
2015-10-23 20:52:10 -0700

    removed images, added final report

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# frequency-fun
-Fun with frequencies
+# README.md for Project 2 
+### Authors: Mike Bartoli and Noah Mulfinger
+
+## General structure
+We don't have the different parts of the project split into differnt functions. At the moment, the parts of the project are meant to be run using the 'run section' feature with matlab.
+
+## File Descriptions
+main.m			The master file, holds all of the parts for project 2
+sharpen.m		Sharpen function from part 0
+normalize.m		Normalizes a 2D image
+normalize.m		Normalizes a 3D image
+laplacianstack.m	Converts a set of guasssian images to laplacian images
+gaussianstack.m		Converst a set of laplacian images to gaussian images
+fft_direction.m		Implements direction based filtering
+fft_high.m		Implements ideal high pass filter
+fft_low.m		Implements ideal low pass filter
--- main.m
+++ main.m
@@ -26,8 +26,8 @@ montage([orig, new], 'Size', [1 1]);
 
 return;
 %% Part 1
-I1 = rgb2gray(imresize(im2double(imread('./Selection_003.png')),  [100 100]));
-I2 = rgb2gray(imresize(im2double(imread('./Selection_004.png')),  [100 100]));
+I1 = rgb2gray(imresize(im2double(imread('./dog.png')),  [100 100]));
+I2 = rgb2gray(imresize(im2double(imread('./cat.png')),  [100 100]));
 
 F1 = fftshift(fft2(I1));
 F2 = fftshift(fft2(I2));
@@ -39,8 +39,8 @@ F2 = fftshift(fft2(I2));
 M1 = abs(F1);
 M2 = abs(F2);
 
-P1 = angle(F1);
-P2 = angle(F2);
+P1 = 2* angle(F1);
+P2 = 2 * angle(F2);
 
 I1 = ifft2(ifftshift(M1 .* exp(1i * P1))); 
 I2 = ifft2(ifftshift(M2 .* exp(1i * P2))); 
@@ -61,19 +61,25 @@ imshow(I2)
 % J = rgb2gray(im2double(imread('./Selection_004.png')));
 
 
-I = im2double(imread('./Selection_003.png'));
-J = rgb2gray(im2double(imread('./Selection_004.png')));
+I = im2double(imread('./dog.png'));
+J = rgb2gray(im2double(imread('./cat.png')));
+
+imwrite(abs(fftshift(fft2(rgb2gray(I)))), 'dogmag.png');
+imwrite(abs(fftshift(fft2(J))), 'catmag.png');
+
 J = cat(3, J, J, J);
 
 
+
+
 figure
-title('Select the eyes');
+title('Select a defined point.');
 imshow(I)
 [x1, y1] = ginput(1);
 close
  
 figure
-title('Select the left eye');
+title('Select where you want to align the previous point in this image.');
 imshow(J)
 [x2, y2] = ginput(1);
 close
@@ -81,81 +87,70 @@ close
 shiftx = floor(x1 - x2);
 shifty = floor(y1 - y2);
  
-J = circshift(J, [shiftx shifty]);
+J = circshift(J, [ shifty shiftx ]);
      
 I = imfilter(I, fspecial('Gaussian',29, 7));
  
-J = J - imfilter(J, fspecial('Gaussian', 9, 2));
+J = J - imfilter(J, fspecial('Gaussian', 18, 2));
+
  
 K = I + J;
 
 figure
 imshow(I)
+%imwrite(I, 'einsteinfiltered.png');
+imwrite(abs(fftshift(fft2(I))), 'dogfmag.png');
+imwrite(abs(fftshift(fft2(J))), 'catfmag.png');
 figure
 imshow(J)
+%imwrite(J, 'monroefiltered.png');
 figure
 imshow(K)
+%imwrite(K, 'einsteinandmonroe.png');
+imwrite(abs(fftshift(fft2(J))), 'dogcatmag.png');
 
 %% Part 3
 
-I = rgb2gray(im2double(imread('./dali.jpg')));
-J = im2double(imread('./dali.jpg'));
+I = rgb2gray(im2double(imread('./cat.png')));
+J = im2double(imread('./cat.png'));
 
-x = 201;
-y = 201;
-
-I1 = imfilter(I, fspecial('Gaussian', [x y], 2));
-I2 = imfilter(I, fspecial('Gaussian', [x y], 4));
-I3 = imfilter(I, fspecial('Gaussian', [x y], 8));
-I4 = imfilter(I, fspecial('Gaussian', [x y], 16));
-I5 = imfilter(I, fspecial('Gaussian', [x y], 32));
- 
 % Black and White 
+[ I1, I2, I3, I4, I5 ] = gaussianstack(I);
+ 
 figure
-montage([I, I1, I2, I3, I4, I5], 'Size', [1 1]);
+M = montage([I, I1, I2, I3, I4, I5], 'Size', [1 1]);
+imwrite(get(M, 'CData'), 'catgaussianBW.png', 'png');
 
-
-L1 = I1 - I2;
-L2 = I2 - I3;
-L3 = I3 - I4;
-L4 = I4 - I5;
-
-% Normalize the images
-L1 = normalize(L1);
-L2 = normalize(L2);
-L3 = normalize(L3);
-L4 = normalize(L4);
-
-
+[ L1, L2, L3, L4 ] = laplacianstack(I1, I2, I3, I4, I5);
 
 figure
 M = montage([I, L1, L2, L3, L4], 'Size', [1 1]);
-
-imwrite(get(M, 'CData'), 'montage.png', 'png');
-
-
-J1 = imfilter(J, fspecial('Gaussian', [x y], 2));
-J2 = imfilter(J, fspecial('Gaussian', [x y], 8));
-J3 = imfilter(J, fspecial('Gaussian', [x y], 16));
-J4 = imfilter(J, fspecial('Gaussian', [x y], 32));
-J5 = imfilter(J, fspecial('Gaussian', [x y], 64));
-
+imwrite(get(M, 'CData'), 'catlaplacianBW.png', 'png');
 
 
 % Color
+[ J1, J2, J3, J4, J5 ] = gaussianstack(J);
+
 figure
-montage([J, J1, J2, J3, J4, J5], 'Size', [1 1]);
+M = montage([J, J1, J2, J3, J4, J5], 'Size', [1 1]);
+imwrite(get(M, 'CData'), 'catgaussianC.png', 'png');
 
 
+[ M1, M2, M3, M4 ] = laplacianstack(J1, J2, J3, J4, J5);
+
+figure
+M = montage([I, L1, L2, L3, L4], 'Size', [1 1]);
+imwrite(get(M, 'CData'), 'catlaplacianC.png', 'png');
 
 
 %% Part 4
 
-I = im2double(imread('./hand.png'));
+I = im2double(imread('./eye.png'));
 
 [ height, width, depth ] = size(I)
 
-J = im2double(imread('./eye.png'));
+J = im2double(imread('./hand.png'));
+
 
 [ h, w, d ] = size(J)
 
@@ -189,15 +184,16 @@ LS5 = R5 .* I5 + (1 - R5) .* J5;
 
 
 figure
-montage([LS1, LS2, LS3, LS4, LS5], 'Size', [1 1]);
+M = montage([LS1, LS2, LS3, LS4 ], 'Size', [1 1]);
+imwrite(imresize(get(M, 'CData'), .5), 'eyehandlaplacian.png', 'png');
 
-LST = LS1 + LS2 + LS3 + LS4 + LS5;
+LST = LS1 + LS2 + LS3 + LS4;
 
-LSTAVG = LST / 5;
 LSTNORM = normalize3(LST);
 
 figure
-montage([LSTAVG, LSTNORM], 'Size', [1 1]);
+imshow(LSTNORM);
+imwrite(LSTNORM, 'eyeinhand.png');
 
 
 
